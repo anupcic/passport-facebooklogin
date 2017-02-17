ee3027a802a324d791707b5dfc5899188567a06c
Anup <anupk1790@gmail.com>
2017-02-17 13:12:19 +0530

    first commit

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    'secretKey': '12345-67890-09876-54321',
+    'mongoUrl' : 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientID: '1315753648462830',
+        clientSecret: '566a8f6f19ce50961f37dc4557b21edc',
+        callbackURL: 'https://localhost:3443/users/facebook/callback'
+    }
+}
